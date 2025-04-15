a5a2383ab6d0f4ef3987c4da4d2ad50b50011d31
Kaveri1950 <neelikaveri3@gmail.com>
2025-04-15 12:34:38 +0530

    Proper package structure for pip installation

--- setup.py
+++ setup.py
@@ -1,21 +1,39 @@
 from setuptools import setup, find_packages
+import pathlib
+
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='scam_cgm',
-    version='0.1.0',
+    version='0.1.0',  
     description='A Python package for SCAM CGM analysis',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Kaveri',  
-    author_email='neelikaveri3@gmail.com',  # ← replace with your email
+    author_email='neelikaveri3@gmail.com',
     url='https://github.com/Kaveri1950/scam_cgm',
-    packages=find_packages(),
+    packages=find_packages(include=['scam_cgm*']),  
     install_requires=[
-        'numpy',
-        'matplotlib'
+        'numpy>=1.21.0', 
+        'matplotlib>=3.5.0',
     ],
+    python_requires='>=3.8', 
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    keywords='astronomy astrophysics CGM',
+    project_urls={
+        'Bug Reports': 'https://github.com/Kaveri1950/scam_cgm/issues',
+        'Source': 'https://github.com/Kaveri1950/scam_cgm',
+    },
 )
